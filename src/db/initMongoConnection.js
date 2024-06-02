3fef8308bde815244ece3d358bd35b1165ccf48a
KorotinI <shobviponimali@gmail.com>
2024-06-02 15:52:57 +0200

    comm 4

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
-import { envVars } from '../constants/envVars.js';
 
 export const initMongoConnection = async () => {
-  const connectionLink = `mongodb+srv://${env(envVars.MONGODB_USER)}:${env(
-    envVars.MONGODB_PASSWORD,
-  )}@${env(envVars.MONGODB_URL)}/${env(
-    envVars.MONGODB_DB,
-  )}?retryWrites=true&w=majority&appName=Cluster0`;
-
   try {
-    await mongoose.connect(connectionLink);
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
     console.log('Mongo connection successfully established!');
-  } catch (error) {
-    console.error(error.message);
-    throw error;
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
 };
