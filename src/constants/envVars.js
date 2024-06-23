901d208fe7d1a5213ec245a2800d34c773e14701
KorotinI <shobviponimali@gmail.com>
2024-06-23 15:01:28 +0200

    comm1

--- src/constants/envVars.js
+++ src/constants/envVars.js
@@ -1,9 +1,14 @@
-export const envVars = {
+import path from 'node:path';
+export const ENV_VARS = {
   PORT: 'PORT',
   MONGODB_USER: 'MONGODB_USER',
   MONGODB_PASSWORD: 'MONGODB_PASSWORD',
   MONGODB_URL: 'MONGODB_URL',
   MONGODB_DB: 'MONGODB_DB',
+
+  APP_DOMAIN: 'APP_DOMAIN',
+  JWT_SECRET: 'JWT_SECRET',
+  ENABLE_CLOUDINARY: 'ENABLE_CLOUDINARY',
 };
 
 export const SORT_ORDER = {
@@ -12,4 +17,24 @@ export const SORT_ORDER = {
 };
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const THIRTY_DAYS = 30 * 24 * 60 * 60 *  1000;
+export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+
+export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
